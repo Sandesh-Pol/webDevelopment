045f9b186b3efd8ec8ba3a273d67376b7ced4475
Sandesh-Pol <sandeshpol268@gmail.com>
2023-07-03 23:49:29 +0530

    final

---
+++ JS/03Intermediaate/04hosting.js
@@ -0,0 +1,28 @@
+tipper("5");
+
+function tipper(a){
+    var bill = parseInt(a);
+    console.log(bill + 5);
+}
+
+
+
+
+var bigTipper = function(a){
+    var bill = parseInt(a);
+    console.log(bill + 15);
+}
+
+bigTipper("200")
+
+console.log(name);
+var name = "Sandesh";
+
+
+function sayName(){
+    var name = "Sandesh ok"
+    console.log(name);
+}
+
+sayName();
+console.log(name);
