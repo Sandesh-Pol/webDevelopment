ac0b041b9956123d49666e6e96670a043a457750
Sandesh-Pol <sandeshpol268@gmail.com>
2023-07-04 15:46:38 +0530

    FINAL

---
+++ JS/03Intermediaate/15thisPart2.js
@@ -0,0 +1,21 @@
+
+var user = {
+    firstName: "Sandesh",
+    courseCount: 4,
+    getCourseCount:function() {
+        console.log("Line 7",this);
+        function sayHello(){
+            console.log("Hello");
+            console.log("Line 10",this);
+        }
+        sayHello();
+    },
+   
+};
+
+user.getCourseCount();
+
+// function sayHello(){
+//     console.log("Hello");
+// }
+
